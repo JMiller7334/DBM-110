3abce7e9343d7a3885f0cca4a034ffe575a5474f
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-02-08 14:24:10 -0600

    added joins, playerhouseData table.

--- game-database-project.sql
+++ game-database-project.sql
@@ -1,11 +1,18 @@
 -- BASE TABLES --
 DROP TABLE IF EXISTS gameData;
-CREATE TABLE gameData (
+CREATE TABLE gameData(
     userId INTEGER NOT NULL,
     money INTEGER DEFAULT 0,
     inv TEXT,
-    ownObj TEXT
-);
+    ownedObj TEXT,
+    ownedObjQuantity INTEGER);
+
+DROP TABLE IF EXISTS playerHouseData;
+CREATE TABLE playerHouseData(
+    userId INTEGER NOT NULL,
+    objName TEXT,
+    objPos TEXT,
+    objRot TEXT);
 
 DROP TABLE IF EXISTS players;
 CREATE TABLE players(
@@ -19,11 +26,26 @@ CREATE TRIGGER onPlayerAdded AFTER INSERT ON players
 BEGIN
     INSERT INTO gameData(userId)
         VALUES(last_insert_rowid());
+        
+    INSERT INTO playerHouseData(userId)
+        VALUES(last_insert_rowid());
     SELECT * FROM gameData;
 END;
 
 -- TASKS --
+-- Players Join
 INSERT INTO players(username)
-    VALUES('jmiller7334');
+    VALUES('player1'), ('player2'), ('player3');
 
-SELECT * FROM players, gameData;
+--UPDATE--
+UPDATE gameData SET OwnedObj = 'iron nails' WHERE userId = (
+SELECT userId from players WHERE username = 'player3');
+
+-- JOINS --
+SELECT * FROM playerHouseData
+    JOIN gameData ON gameData.userId = playerHouseData.userId;
+
+-- SELECT STATEMENT
+SELECT * FROM players;
+SELECT * FROM gameData;
+SELECT * FROM playerhouseData
