f720002372be2dd3dcaa9c7ae85e4e4e520e2e50
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-04-23 19:48:01 -0500

    new folder for mySQL code

---
+++ mySQL Projects/dashboard.sql
@@ -0,0 +1,60 @@
+/*MYSQL - SQL
+this code has been convert from code for SQLite Studio to code meant for mySQL.
+
+PROJECT GOALS:
+create code the runs within mySQL & mySQLWorkBench.
+Learn new syntax for SQL.
+*/
+
+-- CREATE TABLES --
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) DEFAULT 'notEntered',
+    type VARCHAR(255) NOT NULL
+);
+DROP TABLE IF EXISTS usage_data;
+CREATE TABLE usage_data(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    customerId INTEGER NOT NULL,
+    usage_month VARCHAR(255) NOT NULL,
+    customer_usage DOUBLE NOT NULL
+);
+
+-- CREATE TRIGGERS --
+/* NOTE: Delimiter is set as $$ means we are using // to indicate the end of the statement, since this
+trigger uses multiple ;s already requited the delimiter must be set to $$ to indicate the finish of the 
+trigger and should be reverted back to ; after the func/trigger/etc is finished*/
+
+DROP TRIGGER IF EXISTS formatEntry;
+DELIMITER //
+CREATE TRIGGER formatEntry BEFORE INSERT ON customers
+FOR EACH ROW
+BEGIN
+	SET NEW.firstName = LOWER(NEW.firstName);
+    SET NEW.lastName = LOWER(NEW.lastName);
+    SET NEW.address = LOWER(NEW.address);
+    SET NEW.email = REPLACE(NEW.email, ' ', '');
+    SET NEW.email = LOWER(NEW.email);
+    
+    SET NEW.phone = REPLACE(REPLACE(NEW.phone, ' ', ''),'-', '');
+    
+	IF NEW.phone NOT LIKE 'invalid%' AND (CAST(NEW.phone AS UNSIGNED INTEGER) IS NULL OR LENGTH(NEW.phone) != 10) THEN
+		SET NEW.phone = CONCAT('invalid(', NEW.phone, ')');
+	END IF;
+END//
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS formatUsage;
+DELIMITER //
+CREATE TRIGGER formatUsage BEFORE INSERT ON usage_data
+FOR EACH ROW
+BEGIN
+    SET NEW.customer_usage = REPLACE(NEW.customer_usage, NEW.customer_usage, CAST(NEW.customer_usage AS DOUBLE));
+END//
+DELIMITER ;
